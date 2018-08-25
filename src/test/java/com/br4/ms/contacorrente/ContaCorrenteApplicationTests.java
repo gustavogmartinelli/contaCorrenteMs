89f2799118f6d8c1a70745564ee3c9c8c28f1749
Gustavo Martinelli <gustavogmartinelli@gmail.com>
2018-08-25 11:47:16 -0300

    commit incial

---
+++ src/test/java/com/br4/ms/contacorrente/ContaCorrenteApplicationTests.java
@@ -0,0 +1,17 @@
+package com.br4.ms.contacorrente;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContaCorrenteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+ 
