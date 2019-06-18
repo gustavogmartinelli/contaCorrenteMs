7bae87fead400fdb40a5d2bf686a395be0185c4a
gustavo.martinelli <gustavo.martinelli@basis.com.br>
2019-06-18 16:59:21 -0300

    ajustes

--- src/main/java/com/br4/ms/contacorrente/persistencia/entidades/Moeda.java
+++ src/main/java/com/br4/ms/contacorrente/persistencia/entidades/Moeda.java
@@ -12,6 +12,12 @@ import com.br4.ms.contacorrente.persistencia.Entidade;
 @Table(name="Moeda")
 public class Moeda  extends Entidade{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8500552044906748184L;
+	
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
